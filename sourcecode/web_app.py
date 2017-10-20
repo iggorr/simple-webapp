e261fe74f80a5054270242501097ebf1900578b9
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-20 11:20:33 +0000

    Added Test Code

--- sourcecode/web_app.py
+++ sourcecode/web_app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-  return "Main Page"
+  return render_template("main.html")
+
+@app.route("/display/")
+@app.route("/display/<page>")
+def display(page=None):
+  if page == None:
+    return redirect(url_for("root"))
+  else:
+    return "This is a page about %s" % page
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host="0.0.0.0", debug=True)
