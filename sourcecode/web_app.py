6107755be5b0878d45013dc908d72fe628876f5f
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-21 02:16:41 +0000

    Added JSON file for retrieving the data, implemented the looping functionality.

--- sourcecode/web_app.py
+++ sourcecode/web_app.py
@@ -1,18 +1,37 @@
 from flask import Flask, redirect, url_for, render_template
+import json
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-  return render_template("main.html")
+with open('static/data.json') as infile:
+  data = json.load(infile)
+  infile.close()
 
-@app.route("/display/")
-@app.route("/display/<page>")
-def display(page=None):
+@app.route('/')
+def root():
+  return render_template('main.html', data=data)
+
+@app.route('/<category>')
+def display(category):
+  for item in data:
+    if item['url'] == category:
+      return "Yass"
+  return "Item not found babe"
+
+
+
+@app.route('/test')
+def test():
+  print data[1]
+  return "Okie"
+
+@app.route('/display/')
+@app.route('/display/<page>')
+def dispglay(page=None):
   if page == None:
-    return redirect(url_for("root"))
+    return redirect(url_for('root'))
   else:
     return "This is a page about %s" % page
 
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
