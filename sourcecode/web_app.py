7a5fb5bff0ff4934910cc38c3d5948ff94df1932
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-24 19:45:20 +0000

    Added cust om 404 error page. Added displaying functionality for an individual coffee maker. Added more info into the web-app's data

--- sourcecode/web_app.py
+++ sourcecode/web_app.py
@@ -14,47 +14,36 @@ def root():
   return render_template('main.html', data=data)
 
 # Route to a particular maker or type of makers that is passed as a URL variable
-@app.route('/<mtype>')
-@app.route('/<mtype>/<maker>')
-def display(mtype, maker=None):
+@app.route('/<category>')
+@app.route('/<category>/<maker>')
+def display(category, maker=None):
   # Iterating through each maker type in the data
   for item in data:
     # If a maker type with a matching URL has been found
-    if item['url'] == mtype:
+    if item['url'] == category:
       # If a particular maker hasn't been passed as a URL variable, 
       # display the html template with the maker type's details
       if maker == None:
-        return render_template('results.html', result=item) 
+        return render_template('makers.html', result=item)
       # If a particular maker has been specified
       else:
         # Iterate through each make of the specified type
         for machine in item['makers']:
           # If a maker with a matching URL has been found
           if machine['url'] == maker:
-            return "Item found!"
-  # Otherwise, throw a 404 error
+            # Display the html template with the details of the maker
+            return render_template('maker.html', result=machine, cat=item)
+        # If the maker wasn't found in the type's details, return the page
+        # of the maker type instead
+        return redirect(url_for('display', category=category))
+  # If a maker type wasn't found, throw a 404 error
   abort(404)
 
 # Custom defined error for code 404
 @app.errorhandler(404)
 def page_not_found(error):
-  return "The page you have requested wasn't found!", 404
-
-
-
-
-
-@app.route('/display/')
-@app.route('/display/<page>')
-def dispglay(page=None):
-  if page == None:
-    return redirect(url_for('root'))
-  else:
-    return "This is a page about %s" % page
-
-
-
-
+  # Displaying the appropriate template and returning the error code
+  return render_template('404.html'), 404
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
